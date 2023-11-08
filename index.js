0d966b777e03e006037d0182e5061e6f738848ea
sim-sal <simone.saladino93@gmail.com>
2023-11-08 15:05:33 +0100

    strated project and created array object

---
+++ index.js
@@ -0,0 +1,42 @@
+// DEFINISCO L'ARRAY DI OGGETTI CONTENENTE FILM O SERIE TV
+const filmsAndSeries = [
+    {
+        title: "Il Signore degli Anelli",
+        year: 2001,
+        genre: "Fantasy",
+        rating: 9.3,
+        type: "movie",
+    },
+    {
+        title: "The Shawshank Redemption",
+        year: 1994,
+        genre: "Drama",
+        rating: 9.3,
+        type: "movie",
+    },
+    {
+        title: "Inception",
+        year: 2010,
+        genre: "Sci-Fi",
+        rating: 8.8,
+        type: "movie",
+    },
+    {
+        title: "Game of Thrones",
+        year: 2011,
+        genre: "Fantasy",
+        rating: 9.2,
+        type: "tv",
+        seasons: 8,
+    },
+    {
+        title: "Breaking Bad",
+        year: 2008,
+        genre: "Crime",
+        rating: 9.5,
+        type: "tv",
+        seasons: 5,
+    },
+];
+
+console.log(filmsAndSeries);
