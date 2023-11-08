73a0936e8cc6956250c4cccda0f3391e4de890d6
sim-sal <simone.saladino93@gmail.com>
2023-11-08 16:00:52 +0100

    added the classes for films and series with their constructors and printed their properties in the console

--- index.js
+++ index.js
@@ -39,4 +39,43 @@ const filmsAndSeries = [
     },
 ];
 
-console.log(filmsAndSeries);
+// console.log(filmsAndSeries);
+
+// CREO LA CLASSE MOVIE 
+class Movie {
+    constructor(title, year, genre, rating, type) {
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.rating = rating;
+        this.type = type;
+    }
+
+    // Creo il metodo toString() che ritorni una stringa con i dati del film
+    toString() {
+        return `${this.title} è un film di genere ${this.genre}. E' stato rilasciato nel ${this.year} ed ha un voto di ${this.rating}`;
+    };
+}
+
+// Nuova istanza della classe Movie:
+const movie1 = new Movie("Titanic", 1997, "Drama", 7.8, "movie");
+console.log(movie1.toString());
+
+// CREO LA CLASSE TVSERIES CHE ESTENDE MOVIE
+class TvSeries extends Movie {
+    constructor(title, year, genre, rating, type, seasons) {
+        super(title, year, genre, rating, type); // Chiama il costruttore della classe padre
+
+        // aggiungo una nuova chiave
+        this.seasons = seasons;
+    }
+
+    // Creo il metodo toString() che ritorni una stringa con i dati del film
+    toString() {
+        return `${this.title} è una serie tv di genere ${this.genre}. La prima stagione è stata rilasciata nel ${this.year} ed in totale sono state prodotte ${this.seasons} stagioni. Ha un voto di ${this.rating}`;
+    };
+}
+
+// Nuova istanza della classe TvSeries:
+const tvSeries1 = new TvSeries("Stranger Things", 2016, "Science Fiction", 8.7, "tv", 4);
+console.log(tvSeries1.toString());
